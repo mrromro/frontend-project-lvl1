d0e6639b20445d1e3a42619ad320870b4f2d1a37
Roman Rovin <insigniasane@gmail.com>
2021-01-03 19:40:50 +0300

    make brain-calc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,13 +1,54 @@
 #!/usr/bin/env node
 
 import play from './index.js';
+import templates from './templates.js';
+import { getRandomInt } from './utils.js';
 
 const description = 'What is the result of the expression?';
 
+const operations = {
+  '*': {
+    limits: [1, 10],
+    fn: (a, b) => a * b,
+    postprocess: (num) => num,
+  },
+  '-': {
+    limits: [0, 100],
+    fn: (a, b) => a - b,
+    postprocess: (num) => num.sort().reverse(),
+  },
+  '+': {
+    limits: [0, 100],
+    fn: (a, b) => a + b,
+    postprocess: (num) => num,
+  },
+};
+
+function chooseOperation(ops) {
+  return Object.keys(ops)[getRandomInt(0, 2)];
+}
+
+function chooseNumbers(operation) {
+  const { limits, postprocess } = operations[operation];
+  const a = getRandomInt(...limits);
+  const b = getRandomInt(...limits);
+  const result = postprocess([a, b]);
+  return result;
+}
+
+function makeEquation(operation, numbers) {
+  const [a, b] = numbers;
+  return `${a} ${operation} ${b}`;
+}
+
 function* game(count = 3) {
   for (let current = 0; current < count; current += 1) {
-    const question = '';
-    const solution = '';
+    const operation = chooseOperation(operations);
+    const [a, b] = chooseNumbers(operation);
+    const question = templates.question(
+      makeEquation(operation, [a, b]),
+    );
+    const solution = operations[operation].fn(a, b).toString();
     yield {
       question,
       solution,
