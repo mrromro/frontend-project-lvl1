2bfc36ae4cd0e8f28d3ef34432df370c0c7caa93
Roman Rovin <insigniasane@gmail.com>
2021-01-03 18:49:47 +0300

    tiny refactor

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,6 +2,8 @@
 
 import play from './index.js';
 
+const description = 'Answer "yes" if the number is even, otherwise answer "no"';
+
 function getRandomInt(min = 0, max = 100) {
   const range = max + 1 - min;
   const seed = Math.random();
@@ -15,12 +17,13 @@ function isEvenSolution(num) {
 function* game(count = 3) {
   for (let current = 0; current < count; current += 1) {
     const question = getRandomInt();
+    const solution = isEvenSolution(question);
     yield {
       question,
-      solution: isEvenSolution(question),
+      solution,
     };
   }
 }
 
-console.log('Answer "yes" if the number is even, otherwise answer "no"');
+console.log(description);
 play(game());
