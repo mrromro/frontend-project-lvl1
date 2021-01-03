dac85f8a23532befeface3b9764692f855c10fa6
Roman Rovin <insigniasane@gmail.com>
2021-01-03 20:48:38 +0300

    add gcd game

---
+++ bin/games/gcd.js
@@ -0,0 +1,26 @@
+import { gcd, getRandomInt } from '../utils.js';
+
+const description = '';
+
+function getNumbers() {
+  const a = getRandomInt(100, 200);
+  const b = getRandomInt(50, 100);
+  return [a, b];
+}
+
+function* game(count = 3) {
+  for (let current = 0; current < count; current += 1) {
+    const [a, b] = getNumbers();
+    const question = `${a} ${b}`;
+    const solution = gcd(a, b).toString();
+    yield {
+      question,
+      solution,
+    };
+  }
+}
+
+export default {
+  game,
+  description,
+};
